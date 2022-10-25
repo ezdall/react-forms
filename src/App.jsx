46e0bfad00c81d65c9952eb4e534d7d3f1f3cadd
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-25 13:46:41 +0800

    add users, notes route & comp, integrate to /dash

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import DashLayout from './components/dash-layout.comp';
 import Layout from './components/layout.comp';
 import Login from './features/login.comp';
 import Public from './components/public.comp';
+//
 import Welcome from './features/welcome.comp';
+import NoteList from './features/notes-list.comp';
+import UsersList from './features/users-list.comp';
 
 export default function App() {
   return (
@@ -16,6 +19,16 @@ export default function App() {
         <Route path="login" element={<Login />} />
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
+          {/* /dash/notes/:noteId  */}
+          <Route path="notes">
+            <Route index element={<NoteList />} />
+            <Route path=":noteId" element={<div>:noteId</div>} />
+          </Route>
+          {/* /dash/users/:userId  */}
+          <Route path="users">
+            <Route index element={<UsersList />} />
+            <Route path=":userId" element={<div>:userId</div>} />
+          </Route>
         </Route>
       </Route>
     </Routes>
