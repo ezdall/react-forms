9f2fc37c223b6368b2e2c269119cd7b758d59dbe
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-25 13:43:50 +0800

    add welcome for /dash, fix some route errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import DashLayout from './components/dash-layout.comp';
 import Layout from './components/layout.comp';
 import Login from './features/login.comp';
 import Public from './components/public.comp';
+import Welcome from './features/welcome.comp';
 
 export default function App() {
   return (
@@ -13,7 +14,9 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />} />
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+        </Route>
       </Route>
     </Routes>
   );
