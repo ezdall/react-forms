27a24abbe938a218d70851083c721286db851df5
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-25 10:10:06 +0800

    add comps: layout, login, public. create routes at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// comp
+import Layout from './components/layout.comp';
+import Login from './components/login.comp';
+import Public from './components/public.comp';
 
 export default function App() {
-  return <div>app</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  );
 }
