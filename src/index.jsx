77e4c10400cb23628ab0b85dbb90d2e1e74b9a69
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-25 10:05:50 +0800

    add css, img. implement react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // comp
 import App from './App';
@@ -8,7 +9,11 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
