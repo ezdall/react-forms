c9bd1cb3740a46bee4afbe051b57fe1ef31a489f
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-26 08:44:42 +0800

    fix route /users cannot receive data

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   // like axios.
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
   tagTypes: ['Note', 'User'],
   endpoints: builder => ({})
 });
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 //
-import { store } from './store';
+import { store } from './api/store';
 // comp
 import App from './App';
 // style
